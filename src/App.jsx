65fc23acfec1eb2e6922d7ffa4f21160814fa80b
DianaMeryl <119227850+DianaMeryl@users.noreply.github.com>
2024-02-14 23:41:32 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Picture from './components/Picture'
+
+export default function App() {
+  return (
+    <>
+        <Picture />
+    </>
+  )
+}
+
+
---
+++ src/components/Picture.jsx
@@ -0,0 +1,10 @@
+import pict from '/1.jpg'
+
+export default function Picture(){
+    return(
+        <div>
+            <h1>Hello world!!!</h1>
+            <img src={pict} alt={"img1"} />
+        </div>
+    )
+}
